9b75096987d34ead88c75f295e22e31fbb8ead8f
Mousam <mousam.kalita777@gmail.com>
2022-02-28 02:23:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// import drumsound1 from '/sound/tom-1.mp3';
 
 function App() {
+  const playAudio1 = () => {
+    new Audio('/sound/tom-1.mp3').play();
+  }
+  const playAudio2 = () => {
+    new Audio('/sound/tom-2.mp3').play();
+  }
+  const playAudio3 = () => {
+    new Audio('/sound/tom-3.mp3').play();
+  }
+  const playAudio4 = () => {
+    new Audio('/sound/tom-4.mp3').play();
+  }
+  const playAudio5 = () => {
+    new Audio('/sound/tom-1.mp3').play();
+  }
+  const playAudio6 = () => {
+    new Audio('/sound/kick-bass.mp3').play();
+  }
+  const playAudio7 = () => {
+    new Audio('/sound/crash.mp3').play();
+  }
+  const playAudio8 = () => {
+    new Audio('/sound/snare.mp3').play();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container" >
+        <h1 className="flicker neon">DISCO</h1>
+        <img src="/images/drum2.jpg" alt="drum1" />
+        <div className="box1" onClick={playAudio1}></div>
+        <div className="box2" onClick={playAudio2}></div>
+        <div className="box3" onClick={playAudio3}></div>
+        <div className="box4" onClick={playAudio4}></div>
+        <div className="box5" onClick={playAudio5} ></div>
+        <div className="box6" onClick={playAudio6}></div>
+        <div className="box7" onClick={playAudio7}></div>
+        <div className="box8" onClick={playAudio8}></div>
+      </div>
     </div>
   );
 }
